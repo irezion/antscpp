1482a2697d58871470d8e1d951a3a93f5662167a
irezion <133502462+irezion@users.noreply.github.com>
2025-03-30 13:21:59 +0800

    Update Task_S41.cpp

--- Task_S41.cpp
+++ Task_S41.cpp
@@ -2,25 +2,25 @@
 using namespace std;
 
 double squared_sum(double a, double b) {
-    double sum = a + b;
-    return sum * sum;
+  double sum = a + b;
+  return sum * sum;
 }
 
 int main() {
     setlocale(LC_ALL,"russian");
     double num1, num2;
-    cout << "Ââåäèòå ïåðâîå äðîáíîå ÷èñëî: ";
+    cout << "Введите первое дробное число: ";
     cin >> num1;
 
-    cout << "Ââåäèòå âòîðîå äðîáíîå ÷èñëî: ";
+    cout << "Введите второе дробное число: ";
     cin >> num2;
 
     double result = squared_sum(num1, num2);
 
-    cout << "Êâàäðàò ñóììû ÷èñåë " << num1 << " è " << num2 << " ðàâåí: " << result <<
+    cout << "Квадрат суммы чисел " << num1 << " и " << num2 << " равен: " << result <<
     endl;
 
   return 0;
 }
 
-//ðåøèë
+//решил
